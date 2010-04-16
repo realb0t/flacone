1b51cde5200bada25f754c253a1bd638e5530345
realbot <realbot@realbot-laptop.(none)>
2010-04-16 22:57:36 +0400

    Добавил функции для вывода сообщений, предупреждений и ошибок, с возможностью их отключения из конфига

--- scripts/common.js
+++ scripts/common.js
@@ -1,7 +1,10 @@
 var Store = {
   Config: function() {
     return {
-      'version' : '0.0.0-alfa'
+      'version'          : '0.0.0-alfa',
+      'display_debugs'   : false,
+      'display_warnings' : true,
+      'display_errors'   : true
     }
   },
   Locale: function() {
@@ -27,3 +30,22 @@ var Store = {
     }
   }
 };
+
+function debug(){
+  if(console && console.log && Store.Config().display_debugs)
+    return window.console.log.apply(console, arguments);
+};
+
+function warn() {
+  if(console && console.warn && Store.Config().display_warnings)
+    return window.console.warn.apply(console, arguments);  
+}
+
+function error() {
+  if(console && console.error && Store.Config().display_errors)
+    return window.console.error.apply(console, arguments);  
+}
+
+function alarm() {
+  alert.apply(window, arguments.join(' '))
+}
